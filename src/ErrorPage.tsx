7f1b630b78da3c616ea7b077c5af81c3e9e6405e
ASO <Alvarito1624@gmail.com>
2025-02-10 14:44:14 -0400

    errorPage

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Algo salió mal.</h1>
+      <p>Por favor, intenta nuevamente más tarde.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import PageTimer from "./timePage/PageTime.tsx"
+import ErrorPage from './ErrorPage.tsx'
 
 const router =createBrowserRouter([
   {
     path:'/',
-    element:<App />
+    element:<App />,
+    errorElement: <ErrorPage />
   },
   {
     path:'/p',
-    element:<PageTimer/>
+    element:<PageTimer/>,
+    errorElement: <ErrorPage />
   }
 
 ])
